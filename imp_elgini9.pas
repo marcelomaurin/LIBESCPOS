8ba9f9954f9f25401ab9914c0656b0dad698f745
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2021-12-31 16:37:42 -0300

    Add files via upload
    correcoes de codigo de barras 2d

--- imp_elgini9.pas
+++ imp_elgini9.pas
@@ -32,12 +32,18 @@ type
      function Sublinhado(): string;
      function DoubleTexto(): string;
      function Guilhotina(): string;
+     function AcionaGaveta(): string;
      function Barra2D(Info : string): string;
      function Barra1D(Info : string): string;
-     function Imagem(Info : String): string;
+     function loadImagem(X,Y : integer;Info : String): string;
+     function imprimeImagem(X,Y : integer): string;
+     function Centralizado(): string;
+     function PaginaM616(): string;
+     function CorPg618(): string;
+     function HabilitaArmazenaDados(): string;
+     function Armazenadados( Info : string): string;
+     function ImprimeQRCODEArmazenado(): string;
      property Serial : String read getserial  write setserial;
-
-
 end;
 
 var
@@ -75,7 +81,7 @@ end;
 
 function TIMP_ELGINI9.InitPrint(): string;
 begin
-  result := '';
+  result := string(#$1B) + string(#$40);
 end;
 
 function TIMP_ELGINI9.LineText(Info: string): string;
@@ -115,12 +121,43 @@ begin
   result := String(#29)+ String(#86)+ string(#66)+ string(#3);
 end;
 
-function TIMP_ELGINI9.Barra2D(Info: string): string;
+function TIMP_ELGINI9.AcionaGaveta(): string;
 begin
-  result := String(#$0A)+ String(#$1C)+String(#$7D)+String(#$25)+
-            String(char(length(info)))+
+  result := String(#16)+ String(#20)+ string(#1)+ string(#0)+string(#8);
+end;
+
+function TIMP_ELGINI9.Barra2D(Info: string): string;
+var
+  testeqr : string;
+  tamanho : integer;
+  lsb, msb : char;
+  bmsb : shortint;
+begin
+  (*
+  testeqr := 'mjBNoBoQVihMwTzUA+IBlgU12si36ipyVC2L+W5PhCvvqx1xVF/moy4wIzGTfWYpqsMa0641ZBFJFWnWIo78YBBE4m2GwGen84VTPz9iwvs4QkAQdijCiX6TA3wWSjIdiYhqyDroH0IBGnIOWug8ModehDreQFyDdnvAUVItszFVKW12/1JFhu9nxneAAKlJYecY46W3LcOOQSvt1yOcJzM74l+BsmlcYZZVi4I1mEazoj7YzG3Rx64444444';
+  tamanho := length(testeqr)+3;
+  if (tamanho>=256) then
+  begin
+   tamI = tamanho-256;
+   tamF = tamanho/256;
+  end
+   else
+  begin
+   tamI = tamanho;
+   tamF = 0;
+  end;
+
+  result := String(#$1D)+ String(#$28)+String(#$6B)+String(#$03)+
+            String(#$00)+String(#$31)+String(#$43)+
+            String(#$06)+ (*QRSIZE*)
+            String(#$1D)+String(#$28)+String(#$6B)+
+            String(lsb)+String(msb)+
+            String(#$31)+String(#$50)+String(#$30)+
             info+
-            String(#$0A);
+            String(#$1D)+String(#$28)+String(#$6B)+String(#$03)+String(#$00)+
+            String(#$31)+String(#$51)+String(#$30);
+  *)
+  result := '';
 end;
 
 function TIMP_ELGINI9.Barra1D(Info: string): string;
@@ -132,10 +169,74 @@ begin
   ;
 end;
 
-function TIMP_ELGINI9.Imagem(Info: String): string;
+function TIMP_ELGINI9.loadImagem(X,Y : integer; Info: String): string;
+var
+  ret : string;
 begin
-  result := '';
+  ret := string(#29)+String(#40)+String(#76)+String(char(X))+String(char(Y))+String(#48)+
+            String(#67)+String(#48)+String(#100)+String(#100)+String(#1)+
+            String(#100)+String(#100)+String(#10)+String(#10)+String(#10) +
+            info + String(#1);
+
+
+  result := ret;
 end;
+
+function TIMP_ELGINI9.imprimeImagem(X,Y : integer): string;
+begin
+     result := string(#29)+ String(#40)+String(#76)+String(char(X))+String(char(Y))+
+            String(#48)+String(#69)+String(#100)+String(#100)+String(char(x))+String(char(y));
+end;
+
+function TIMP_ELGINI9.Centralizado(): string;
+begin
+     result :=  string(#27) + string(#97)+ string(#49);
+end;
+
+function TIMP_ELGINI9.PaginaM616(): string;
+begin
+  result := string(#29)+ string(#40)+ string(#107) + string(#3) + string(#0) + string(#49) + string(#67)+
+         String(#4);
+end;
+
+function TIMP_ELGINI9.CorPg618(): string;
+begin
+   result := string(#29) + string(#40) + string(#107) + String(#3) + string(#0) +
+          string(#49) + string(#69) + string(#48);
+end;
+
+function TIMP_ELGINI9.HabilitaArmazenaDados(): string;
+begin
+  result := string(#29) + string(#40) + string(#107);
+end;
+
+function TIMP_ELGINI9.Armazenadados(Info: string): string;
+var
+  tamanho : integer;
+  tamI: integer;
+  tamF: integer;
+begin
+  tamanho := length(Info)+3;
+  if (tamanho>=256) then
+  begin
+    tamI := integer(tamanho-256);
+    tamF := integer(tamanho div 256);
+  end
+  else
+  begin
+    tamI := tamanho;
+    tamF := 0;
+  end;
+  result :=  string(chr(tamI)) + string(char(tamF)) + string(#49) + string(#80) +
+     string(#48)+info;
+end;
+
+function TIMP_ELGINI9.ImprimeQRCODEArmazenado(): string;
+begin
+  result :=  string(#29) + string(#40) + string(#107) + String(#3) +
+    string(#0) + string(#49) + string(#81) + string(#48);
+end;
+
 
 end.
 
